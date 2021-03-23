024c88f3a2b004c18047c74fcdb915d1aeb79a6b
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-23 15:50:18 +0530

    Changed text and bg color in footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,51 +1,51 @@
 <footer class="footer">
-    <div class="bg-gray-900">
+    <div class="bg-gray-50">
         <div class="container mx-auto">
             <div class="flex flex-wrap justify-between px-4 py-8">
                 <div class="mt-5">
-                    <h4 class="text-gray-100 text-sm font-semibold font-Rubik uppercase">Contact Information</h4>
+                    <h4 class="text-gray-900 text-sm font-semibold font-Rubik uppercase">Contact Information</h4>
 
-                    <p class="mt-5 text-gray-300 text-sm">Call Us 24/7 Free</p>
+                    <p class="mt-5 text-gray-600 text-sm">Call Us 24/7 Free</p>
 
                     <div class="text-2xl tracking-wide text-blue-500 font-bold my-3">+91 98765 43210</div>
 
                     <div class="my-3 text-sm">
-                        <a href="mailto:support&#64;example.com" class="text-gray-300 hover:text-gray-400 focus:text-gray-400 focus:outline-none transition ease-in-out duration-150" title="Support E-Mail">support&#64;example.com</a>
+                        <a href="mailto:support&#64;example.com" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150" title="Support E-Mail">support&#64;example.com</a>
                     </div>
 
                     <div class="mt-3 text-sm">
-                        <addr class="text-gray-300">4, Station Road, Fort, Mumbai - 400001, India.</addr>
+                        <addr class="text-gray-600">4, Station Road, Fort, Mumbai - 400001, India.</addr>
                     </div>
                 </div>
 
                 <div class="mt-5">
-                    <h4 class="text-gray-100 text-sm font-semibold font-Rubik uppercase">Company</h4>
+                    <h4 class="text-gray-900 text-sm font-semibold font-Rubik uppercase">Company</h4>
 
                     <ul class="mt-5">
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">About Us</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Cart</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Track Your Order</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Work With Us</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Contact Us</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">About Us</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Cart</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Track Your Order</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Work With Us</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Contact Us</a></li>
                     </ul>
                 </div>
 
                 <div class="mt-5">
-                    <h4 class="text-gray-100 text-sm font-semibold font-Rubik uppercase">Explore</h4>
+                    <h4 class="text-gray-900 text-sm font-semibold font-Rubik uppercase">Explore</h4>
 
                     <ul class="mt-5">
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Gift Product</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Returns Policy</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">F. A. Qs</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Privacy Policy</a></li>
-                        <li class="text-sm my-3"><a href="#" class="text-gray-300 hover:text-gray-200 focus:text-gray-200 focus:outline-none transition ease-in-out duration-150">Terms &amp; Conditions</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Gift Product</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Returns Policy</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">F. A. Qs</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Privacy Policy</a></li>
+                        <li class="text-sm my-3"><a href="#" class="text-gray-600 hover:text-blue-500 focus:text-blue-500 focus:outline-none transition ease-in-out duration-150">Terms &amp; Conditions</a></li>
                     </ul>
                 </div>
 
                 <div class="mt-5">
-                    <h4 class="text-gray-100 text-sm font-semibold font-Rubik uppercase">Our Location</h4>
+                    <h4 class="text-gray-900 text-sm font-semibold font-Rubik uppercase">Our Location</h4>
 
-                    <img src="{{ asset('/images/footer-location-banner.png') }}" alt="{{ config('app.name') }} Location" title="{{ config('app.name') }} Location" class="mt-5" />
+                    <img src="{{ asset('/images/bg-map-footer.jpg') }}" alt="{{ config('app.name') }} Location" title="{{ config('app.name') }} Location" class="mt-5" />
                 </div>
             </div>
         </div>
@@ -59,7 +59,7 @@
                 </div>
 
                 <div class="mt-2 sm:mt-0 text-gray-400">
-                    Built with <span class="text-blue-500 mx-1"><i class="fas fa-heart"></i></span> using Laravel v{{ app()->version() }} by <a href="https://github.com/MehulBawadia" class="hover:text-gray-300 focus:text-gray-300 focus:outline-none transition ease-in-out duration-150" target="_blank" rel="noopener">Mehul Bawadia</a>
+                    Built with <span class="text-blue-500 mx-1"><i class="fas fa-heart"></i></span> using Laravel v{{ app()->version() }} by <a href="https://github.com/MehulBawadia" class="hover:text-gray-900 focus:text-gray-900 focus:outline-none transition ease-in-out duration-150" target="_blank" rel="noopener">Mehul Bawadia</a>
                 </div>
                 <div class="mt-2 sm:mt-0">
                     <img src="{{ asset('/images/payment.png') }}" alt="Supported Payments" title="Supported Payments" />
