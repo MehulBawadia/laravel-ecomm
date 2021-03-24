0dc0aebfcc5566871b1b60bda1a800dff0a56ab8
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-24 14:22:31 +0530

    Added product information

--- resources/views/pages/products/show.blade.php
+++ resources/views/pages/products/show.blade.php
@@ -130,10 +130,159 @@
             </div>
         </div>
     </section>
+
+    <section class="productInformation">
+        <div class="container mx-auto">
+            <div class="px-4 py-16 tabs">
+                <ul class="flex flex-col sm:flex-row items-center justify-center gap-6">
+                    <li>
+                        <a href="#description" class="leading-9 text-xl font-Rubik font-bold uppercase text-blue-500 block hover:text-blue-500 focus:outline-none">Description</a>
+                    </li>
+                    <li>
+                        <a href="#reviews" class="leading-9 text-xl font-Rubik font-bold uppercase text-gray-500 block hover:text-blue-500 focus:outline-none">Reviews</a>
+                    </li>
+                </ul>
+
+                <div id="description" class="text-gray-500 py-8 leading-relaxed space-y-4">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, corporis atque illum dolore voluptates illo repellat adipisci magnam iusto quia ipsam voluptatibus eveniet facilis eius unde placeat necessitatibus, ipsa dolores! Rerum alias nulla, optio laboriosam quasi facere vero, eligendi amet suscipit autem nihil sit, eveniet animi, dolores error placeat porro aspernatur provident corporis! Rerum consequuntur consectetur reiciendis, odit, dignissimos quasi quisquam. Placeat a corporis suscipit minus illo fugiat rem quod. Reprehenderit perspiciatis eaque eos ducimus et. Sit aperiam, maiores harum.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum, corporis atque illum dolore voluptates illo repellat adipisci magnam iusto quia ipsam voluptatibus eveniet facilis eius unde placeat necessitatibus, ipsa dolores! Rerum alias nulla, optio laboriosam quasi facere vero, eligendi amet suscipit autem nihil sit, eveniet animi, dolores error placeat porro aspernatur provident corporis! Rerum consequuntur consectetur reiciendis, odit, dignissimos quasi quisquam. Placeat a corporis suscipit minus illo fugiat rem quod. Reprehenderit perspiciatis eaque eos ducimus et. Sit aperiam, maiores harum.
+                    </p>
+                </div>
+                <div id="reviews" class="py-8">
+                    <div class="flex justify-between gap-8">
+                        <div class="w-full md:w-1/2">
+                            <h3 class="font-Rubik font-bold text-xl uppercase leading-6 mt-2 sm:mt-0">
+                                2 Reviews
+                            </h3>
+
+                            <div class="flex mt-8">
+                                <div class="flex-none">
+                                    <img src="{{ asset('/images/user-default.jpg') }}" alt="User Full Name" title="User Full Name" class="rounded" />
+                                </div>
+                                <div class="ml-3">
+                                    <div>
+                                        <span class="text-gray-900">User Full Name</span>
+                                        <span class="text-gray-500">has given</span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span class="text-gray-500 text-sm">on {{ now()->format('d M Y, h:i A') }}</span>
+                                    </div>
+
+                                    <div class="mt-3 text-gray-500">
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Soluta quo iure sint, aut ad laboriosam quasi labore reprehenderit voluptatem, omnis sit beatae harum optio dolore est quia quis in odit?
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="flex mt-8">
+                                <div class="flex-none">
+                                    <img src="{{ asset('/images/user-default.jpg') }}" alt="User Full Name" title="User Full Name" class="rounded" />
+                                </div>
+                                <div class="ml-3">
+                                    <div>
+                                        <span class="text-gray-900">User Full Name</span>
+                                        <span class="text-gray-500">has given</span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span><i class="fas fa-star text-blue-500"></i></span>
+                                        <span class="text-gray-500 text-sm">on {{ now()->format('d M Y, h:i A') }}</span>
+                                    </div>
+
+                                    <div class="mt-3 text-gray-500">
+                                        Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Soluta quo iure sint, aut ad laboriosam quasi labore reprehenderit voluptatem, omnis sit beatae harum optio dolore est quia quis in odit?
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-full md:w-1/2">
+                            <h3 class="font-Rubik font-bold text-xl uppercase leading-6 mt-2 sm:mt-0">
+                                Add Your Review
+                            </h3>
+
+                            <p class="text-gray-500 mt-8">Your email address will not be published. Required fields are marked *</p>
+
+                            <form action="#" method="POST" class="mt-8">
+                                @csrf
+
+                                <div class="flex">
+                                    <label class="text-gray-500">Your Rating:</label>
+
+                                    <div class="flex items-center ml-3 text-sm">
+                                        <input type="radio" name="rating" value="1" id="1_star">
+                                        <label for="1_star" class="text-blue-500 ml-1"><i class="fas fa-star"></i></label>
+                                    </div>
+
+                                    <div class="flex items-center ml-3 text-sm">
+                                        <input type="radio" name="rating" value="2" id="2_star">
+                                        <label for="2_star" class="text-blue-500 ml-1">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+
+                                    <div class="flex items-center ml-3 text-sm">
+                                        <input type="radio" name="rating" value="3" id="3_star">
+                                        <label for="3_star" class="text-blue-500 ml-1">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+
+                                    <div class="flex items-center ml-3 text-sm">
+                                        <input type="radio" name="rating" value="4" id="4_star">
+                                        <label for="4_star" class="text-blue-500 ml-1">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+
+                                    <div class="flex items-center ml-3 text-sm">
+                                        <input type="radio" name="rating" value="5" id="5_star">
+                                        <label for="5_star" class="text-blue-500 ml-1">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <div class="w-full">
+                                    <textarea name="review" id="review" class="mt-5 resize-none border px-5 py-2 focus:outline-none focus:ring w-full" placeholder="Your Review *" rows="8">{{ old('review') }}</textarea>
+                                </div>
+
+                                <div class="flex justify-center items-center gap-6 mt-5">
+                                    <div class="w-1/2">
+                                        <input type="text" name="full_name" id="fullName" class="border px-5 py-2 focus:outline-none focus:ring w-full" placeholder="Full Name *" />
+                                    </div>
+                                    <div class="w-1/2">
+                                        <input type="email" name="email" id="email" class="border px-5 py-2 focus:outline-none focus:ring w-full" placeholder="E-Mail *" />
+                                    </div>
+                                </div>
+
+                                <button type="submit" class="w-full mt-5 py-3 font-Rubik text-sm tracking-wider uppercase bg-blue-500 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none">Submit Review</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
 
 @section('pageScripts')
     <script src="https://cdn.rawgit.com/igorlino/elevatezoom-plus/1.1.6/src/jquery.ez-plus.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tabslet.js/1.7.3/jquery.tabslet.min.js"></script>
     <script>
         var productImage = $("#mainProductImage");
 
@@ -146,5 +295,21 @@
             containLensZoom: true,
             gallery: "productGallery",
         });
+
+        $('.tabs ul li a').on('click', function (e) {
+            var self = $(this).attr('href');
+
+            $('.tabs ul li a').each(function (index, elem) {
+                var element = $(elem);
+
+                if (element.attr('href') === self) {
+                    element.addClass('text-blue-500').removeClass('text-gray-500');
+                } else {
+                    element.removeClass('text-blue-500').addClass('text-gray-500');
+                }
+            });
+        });
+
+        $('.tabs').tabslet();
     </script>
 @endsection
