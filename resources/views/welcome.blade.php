c816e5ade7a0197a1c06b6d7436738642a662d64
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-24 14:38:51 +0530

    Links to categories page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,27 +29,27 @@
                 <div class="my-8">
                     <img src="{{ asset('/images/categories/category-image-1.jpg') }}" alt="Category 1" title="Category 1" class="block mx-auto hover:animate-pulse transition ease-in-out duration-150" />
 
-                    <a href="#" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 1</a>
+                    <a href="{{ route('categories.show', 'category-1') }}" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 1</a>
                 </div>
                 <div class="my-8">
                     <img src="{{ asset('/images/categories/category-image-2.jpg') }}" alt="Category 2" title="Category 2" class="block mx-auto hover:animate-pulse transition ease-in-out duration-150" />
 
-                    <a href="#" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 2</a>
+                    <a href="{{ route('categories.show', 'category-1') }}" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 2</a>
                 </div>
                 <div class="my-8">
                     <img src="{{ asset('/images/categories/category-image-3.jpg') }}" alt="Category 3" title="Category 3" class="block mx-auto hover:animate-pulse transition ease-in-out duration-150" />
 
-                    <a href="#" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 3</a>
+                    <a href="{{ route('categories.show', 'category-1') }}" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 3</a>
                 </div>
                 <div class="my-8">
                     <img src="{{ asset('/images/categories/category-image-4.jpg') }}" alt="Category 4" title="Category 4" class="block mx-auto hover:animate-pulse transition ease-in-out duration-150" />
 
-                    <a href="#" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 4</a>
+                    <a href="{{ route('categories.show', 'category-1') }}" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 4</a>
                 </div>
                 <div class="my-8">
                     <img src="{{ asset('/images/categories/category-image-5.jpg') }}" alt="Category 5" title="Category 5" class="block mx-auto hover:animate-pulse transition ease-in-out duration-150" />
 
-                    <a href="#" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 5</a>
+                    <a href="{{ route('categories.show', 'category-1') }}" class="block mt-2 text-gray-900 text-center hover:text-blue-500 font-Rubik font-semibold text-sm focus:outline-none focus:text-blue-500 transition ease-in-out duration-150">Category Name 5</a>
                 </div>
             </div>
         </div>
