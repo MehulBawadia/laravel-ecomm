e8602ea92b91351d13ac503be603d7f496322bb4
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-30 16:24:44 +0530

    Changed margin and removed focus ring

--- resources/views/partials/_logo_and_search.blade.php
+++ resources/views/partials/_logo_and_search.blade.php
@@ -5,9 +5,9 @@
                 <a href="{{ route('homePage') }}" class="text-4xl font-bold text-white tracking-wide hover:text-blue-900 focus:text-blue-900 focus:outline-none transition ease-in-out duration-150" title="Go to Home Page">{{ config('app.name') }}.</a>
             </div>
             <div class="mt-5 sm:mt-0 relative">
-                <input type="search" class="rounded py-2 pl-8 w-64 sm:w-96 focus:outline-none focus:ring" placeholder="Search" />
+                <input type="search" class="rounded py-2 pl-8 w-64 sm:w-96 focus:outline-none" placeholder="Search" />
 
-                <div class="absolute top-0 left-0 mt-2 ml-2 text-gray-400"><i class="fas fa-search"></i></div>
+                <div class="absolute top-0 left-0 mt-3 ml-2 text-gray-400"><i class="fas fa-search"></i></div>
             </div>
             <div class="mt-5 sm:mt-0">
                 <a href="{{ route('cart.index') }}" class="text-xl text-white hover:text-blue-900 focus:text-blue-900 focus:outline-none transition ease-in-out duration-150" title="Cart"><i class="fas fa-shopping-cart"></i> : <span class="px-1">2</span></a>
