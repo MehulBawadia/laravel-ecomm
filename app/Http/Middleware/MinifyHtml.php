cc3ac7d63f44ed244072b82828778051efcb7fa0
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-30 15:56:29 +0530

    Minifying html when in production

---
+++ app/Http/Middleware/MinifyHtml.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class MinifyHtml
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response->getStatusCode() == 200 && app()->environment() == 'production') {
+            return $this->html($response);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Get the html content from the given response.
+     *
+     * @param  mixed $response
+     * @return \Illuminate\Http\Response
+     */
+    public function html($response)
+    {
+        $buffer = $response->getContent();
+
+        $replace = [
+            '/<!--[^\[](.*?)[^\]]-->/s' => '',
+            "/<\?php/"                  => '<?php ',
+            "/\n([\S])/"                => '$1',
+            "/\r/"                      => '',
+            "/\n/"                      => '',
+            "/\t/"                      => '',
+            '/ +/'                      => ' ',
+        ];
+
+        if (false !== strpos($buffer, '<pre>')) {
+            $replace = [
+                '/<!--[^\[](.*?)[^\]]-->/s' => '',
+                "/<\?php/"                  => '<?php ',
+                "/\r/"                      => '',
+                "/>\n</"                    => '><',
+                "/>\s+\n</"                 => '><',
+                "/>\n\s+</"                 => '><',
+            ];
+        }
+
+        $buffer = preg_replace(array_keys($replace), array_values($replace), $buffer);
+
+        $response->setContent($buffer);
+
+        return $response;
+    }
+}
