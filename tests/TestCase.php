d3d58c2d1d65ae020165dce631356bd83fd1263b
Mehul Bawadia <mehulbawadia@gmail.com>
2021-09-07 17:09:18 +0530

    Added default admin methods for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,54 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Create a dummy super administrator.
+     *
+     * @return \App\Models\User
+     */
+    public function dummySuperAdministrator()
+    {
+        return User::factory()->create([
+            'first_name'       => 'Super',
+            'last_name'        => 'Administrator',
+            'username'         => 'admin',
+            'email'            => 'admin@example.com',
+            'password'         => bcrypt('Password'),
+        ]);
+    }
+
+    /**
+     * Sign in the administrator.
+     *
+     * @return  \App\Models\User
+     */
+    public function signInAdministrator()
+    {
+        return $this->be(
+            $this->dummySuperAdministrator()
+        );
+    }
+
+    /**
+     * Get the list of invalid E-Mail Addresses.
+     *
+     * @return  array
+     */
+    protected function getInvalidEmailAddress()
+    {
+        return array_rand([
+            "plainaddress", "#@%^%#$@#$@#.com", "@example.com", "Joe Smith-<email@example.com>",
+            "email.example.com", "email@example@example.com", ".email@example.com", "email.@example.com",
+            "email..email@example.com", "email@example.com...(Joe Smith)",
+            "email@-example.com", "email@example..com",
+            "Abc..123@example.com"
+        ]);
+    }
 }
