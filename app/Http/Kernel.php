53692577f46f1bc43da6252792cd77c73d851ba2
Mehul Bawadia <mehulbawadia@gmail.com>
2021-09-07 18:32:58 +0530

    Admin logged in middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'adminExists' => \App\Http\Middleware\AdminExists::class,
+        'adminLoggedIn' => \App\Http\Middleware\AdminLoggedIn::class,
     ];
 }
