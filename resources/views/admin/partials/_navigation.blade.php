f837eefd253466f2fc59a08483d77feed416ac4b
Mehul Bawadia <mehulbawadia@gmail.com>
2021-09-21 14:39:22 +0530

    Logo in fixed position

--- resources/views/admin/partials/_navigation.blade.php
+++ resources/views/admin/partials/_navigation.blade.php
@@ -1,9 +1,9 @@
 <div class="hidden md:block md:w-1/4 lg:w-1/6 h-screen bg-white border-r navigationMenu">
-    <div class="pt-5 text-center">
+    <div class="py-4 fixed text-center border-b md:w-1/4 lg:w-1/6 bg-white">
         <a href="{{ route('homePage') }}" class="text-2xl font-bold text-gray-800 tracking-wide hover:text-indigo-600 focus:text-indigo-600 focus:outline-none transition ease-in-out duration-150" target="_blank" title="Go to Home Page"><span class="text-2xl"><i class="fas fa-shopping-cart"></i></span> {{ config('app.name') }}</a>
     </div>
 
-    <ul class="mt-5 space-y-2">
+    <ul class="mt-24 space-y-2">
         <li>
             <a href="{{ route('admin.dashboard') }}" class="block @if (request()->url() === route('admin.dashboard')) bg-blue-100 text-indigo-600 border-l-4 border-blue-600 @else text-gray-500 @endif py-2 px-3 tracking-wide hover:text-indigo-600 hover:bg-blue-100 border-l-4 border-transparent hover:border-blue-600 focus:text-indigo-600 focus:border-blue-600 focus:bg-blue-100 focus:outline-none transition ease-in-out duration-150" title="Go to Dashboard"><span class="w-6 inline-block"><i class="fas fa-home text-lg"></i></span> <span class="ml-2">Dashboard</span></a>
         </li>
