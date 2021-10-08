c3a4358393c1d5f74f105f97385cdf3de3d6d33b
Mehul Bawadia <mehulbawadia@gmail.com>
2021-10-08 18:49:36 +0530

    Fixed alignment issue in coupons

--- resources/views/admin/coupons/edit.blade.php
+++ resources/views/admin/coupons/edit.blade.php
@@ -22,7 +22,7 @@
 @endsection
 
 @section('content')
-    <section class="generalInformation px-10 mt-8 pt-16">
+    <section class="generalInformation mt-8 pt-16 px-6">
         <div class="container mx-auto">
             <h2 class="text-xl font-medium tracking-wide">General Information</h2>
 
