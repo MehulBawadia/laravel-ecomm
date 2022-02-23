02527d90a6d266a9c025fe850cdfc9546498695f
Mehul Bawadia <mehulbawadia@gmail.com>
2022-02-23 21:34:29 +0530

    Upgraded tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,10 @@
 module.exports = {
-  purge: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        gray: {
-          150: '#f3f4f6'
-        }
-      }
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [
